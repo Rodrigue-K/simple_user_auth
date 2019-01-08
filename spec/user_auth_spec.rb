9b7036260a9b7439b772de5be5663b994526883a
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-08 15:34:32 +0000

    All three unit tests added to check user existence

--- spec/user_auth_spec.rb
+++ spec/user_auth_spec.rb
@@ -1,13 +1,18 @@
 require 'user'
 
 describe User do
-   let(:user) { User.new(email: 'test@test.com', password: 'Password') }
 
-  it 'should check if a user exists when signing in' do
+  it "check a user's existence" do
     expect(User.authenticate('testing@gmail.com', '123456')).to eq (nil)
   end
 
-  it "should check that the user's password is correct when signing in" do
+  it "check that the user's password is correct" do
     expect(User.authenticate("test@test.com", "Password")).to eq (nil)
   end
+
+  it "check that the username and password are both correct" do
+    user = User.create(email: "test@test.com", password: "Password")
+    expect(User.authenticate("test@test.com", "Password")).to eq (user)
+  end
+
 end
