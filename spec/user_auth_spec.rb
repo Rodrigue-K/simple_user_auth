832b75cae913385967b0279346823ade9eac48a4
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-08 15:16:39 +0000

    Second unit test - check user correct but password incorrect

--- spec/user_auth_spec.rb
+++ spec/user_auth_spec.rb
@@ -3,7 +3,11 @@ require 'user'
 describe User do
    let(:user) { User.new(email: 'test@test.com', password: 'Password') }
 
-  it 'should return user to sign up page if user does' do
+  it 'should check if a user exists when signing in' do
     expect(User.authenticate('testing@gmail.com', '123456')).to eq (nil)
+  end
+
+  it "should check that the user's password is correct when signing in" do
+    expect(User.authenticate("test@test.com", "Password")).to eq (nil)
   end
 end
